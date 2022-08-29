57d2aadab503f8195d62aa07763df522f38b4744
DaniloYaremenko <danilyaremenko@gmail.com>
2022-08-29 03:22:37 +0300

    feat: add basic page layout

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import Appinfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+    return (
+        <div className="app">
+            <Appinfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList/>
+            <EmployeesAddForm/>
+        </div>
+    );
+}
+
+export default App;
