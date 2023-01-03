77ee5d820dfe669f4e88356bad72b606e6473ca3
DaniloYaremenko <danilyaremenko@gmail.com>
2023-01-03 03:12:54 +0200

    add search and filter functionality

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const Appinfo = () => {
+const Appinfo = ({ increasedEmployees, employees }) => {
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {employees}</h2>
+            <h2>Премию получат: {increasedEmployees}</h2>
         </div>
     );
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,10 +13,12 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: "John", salary: 800, increase: false, id: 1},
-                {name: "Terentiy", salary: 3000, increase: false, id: 2},
-                {name: "Helicopter", salary: 5000, increase: false, id: 3},
-            ]
+                {name: "John", salary: 800, increase: false, rise: true, id: 1},
+                {name: "Terentiy", salary: 3000, increase: true, rise: false, id: 2},
+                {name: "Helicopter", salary: 5000, increase: false, rise: false, id: 3},
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -34,8 +36,14 @@ class App extends Component {
             name,
             salary,
             increase: false,
+            rise: false,
             id: this.maxId++
         }
+
+        if (name.length < 3 || !salary > 0) {
+            return;
+        }
+
         this.setState(({data}) => {
             const newArr = [...data, newItem];
             return {
@@ -45,19 +53,68 @@ class App extends Component {
         
     }
 
+    onToggleProp = (id, prop) => {
+
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+    searchEmployee = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1 
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;    
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
+        const { data, term, filter } = this.state;
+        const employees = this.state.data.length;
+        const increasedEmployees = this.state.data.filter(item => item.increase).length
+        const visibleData = this.filterPost(this.searchEmployee(data, term), filter);
+
         return (
             <div className="app">
-                <Appinfo/>
+                <Appinfo
+                    employees={employees}
+                    increasedEmployees={increasedEmployees}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployeesList 
-                    data={this.state.data} 
-                    onDelete={this.deleteItem}/>
+                    data={visibleData} 
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
