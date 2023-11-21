f6d6261b75c736834b4aafed47817188645f4903
DaniloYaremenko <danilyaremenko@gmail.com>
2023-11-21 21:15:14 +0200

    feat: switch to english localization

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,9 +3,9 @@ import './app-info.css';
 const Appinfo = ({ increasedEmployees, employees }) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании</h1>
-            <h2>Общее число сотрудников: {employees}</h2>
-            <h2>Премию получат: {increasedEmployees}</h2>
+            <h1>Employee accounting in the company</h1>
+            <h2>Total number of employees: {employees}</h2>
+            <h2>Bonus will receive: {increasedEmployees}</h2>
         </div>
     );
 }
